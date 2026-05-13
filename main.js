ea6f59915d6eec2eee64945e61b7d743b4621442
thomaslazzeri3 <thomaslazzeri3@gmail.com>
2026-05-13 23:00:59 +0200

    add content js

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-for (let i = 0; i < 100; i++){
-console.log(i + 1);
+for (let i = 0; i <= 100; i++) {
+    console.log(i + 1);
+
+    if (i % 3 === 0) {
+        console.log("Fizz");
+    }
+
+    else if (i % 5 === 0) {
+        console.log("Buzz");
+    }
+
+    else if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    }
 }
+
